1f994db7035bcaffdb7526fcb89f73e542bd6706
Youri <yyourii164@gmail.com>
2024-08-15 11:47:03 +0900

    MLB hitter 코드 수정(url 추가 및 페이지 스크랩 사이 시간 추가

--- Youri/MLB_hitter.py
+++ Youri/MLB_hitter.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from team3_module import save_to_csv
+import random
+import time
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
@@ -17,6 +19,7 @@ def scrape_data(urls, name_selector, table_selector):
             cells = [cell.text.strip() for cell in row.find_all('td')]
             if cells:
                 data.append([name] + cells)
+        time.sleep(random.randint(3, 6))
     return data
 
 def get_headers(url, headers_selector):
@@ -26,11 +29,20 @@ def get_headers(url, headers_selector):
     return ["name"] + header_cells
 
 retire_hitter_url = [
-    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=64300"
+    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=71564",
+    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=77623",
+    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=71752",
+    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=70756",
+    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=97109",
+    "https://www.koreabaseball.com/Record/Retire/Hitter.aspx?playerId=95436"
 ]
 
 hitter_url = [
     "https://www.koreabaseball.com/Record/Player/HitterDetail/Total.aspx?playerId=76290",
+    "https://www.koreabaseball.com/Record/Player/HitterDetail/Total.aspx?playerId=75125",
+    "https://www.koreabaseball.com/Record/Player/HitterDetail/Total.aspx?playerId=51817",
+    "https://www.koreabaseball.com/Record/Player/HitterDetail/Total.aspx?playerId=76313",
+    "https://www.koreabaseball.com/Record/Player/HitterDetail/Total.aspx?playerId=50848",
 ]
 
 retire_hitter_data = scrape_data(retire_hitter_url, "#cphContents_cphContents_cphContents_ucRetireInfo_lblName", "#contents > div.sub-content > div.player_records > table.tData01.tt.mb5 > tbody")
